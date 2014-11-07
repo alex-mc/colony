6720ac4143c62f9b16438875e9b3470b54357423
Alex <mcmaster2010@gmail.com>
2014-11-06 22:14:06 -0800

    initial commit

---
+++ picker_classes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 21:48:06 2014
+
+@author: Alex
+"""
+
+class Picker():
+    """Superclass for all pickers."""
+    
+    def __init__(self):
+        None
+    
+    def pick(self):
+        None
