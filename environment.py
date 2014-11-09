2d388546591bab7b59dbc2952c81bd06b6b5149d
alex-mc <mcmaster2010@gmail.com>
2014-11-08 20:29:28 -0800

    created environment

--- environment.py
+++ environment.py
@@ -1,96 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov  7 13:39:02 2014
+Created on Fri Nov  7 16:30:50 2014
 
 @author: Alex
 """
 
-from values import symbol_list
-import ystockquote as ys
+from environment_methods import *
+from picker_classes import *
 
-def get_morning_data():
-    """Retrieve all data available from ystockquote for each known symbol. This
-    data is classified by pickers and added to past data once evening prices
-    are obtained.  This method should be run in the morning to allow pickers
-    to make their picks."""
-    current_data = {}
-    for symbol in symbol_list:
-        print(symbol)
-        current_data[symbol] = ys.get_all(symbol)
-    return current_data
+try:
+    past_data, past_performance = get_past_data()
+except:
+    morning_data = get_morning_data()
+    evening_prices = get_evening_prices()
+    symbol_performances = evaluate_symbols(morning_data, evening_prices)
+    update_past_data()
+    past_data, past_performance = get_past_data()
 
-def get_evening_prices():
-    """Retrieve price for each symbol.  This method should be run in the
-    evening so that picker's picks can be evaluated."""
-    new_data = {}
-    for symbol in symbol_list:
-        print(symbol)
-        new_data[symbol] = ys.get_price(symbol)
-    return new_data
-
-def evaluate_symbols(current_data, new_data):
-    """Assign a number from -2 to 2 to each symbol depending on its
-    performance from the past day.
-    
-    2   -  better than 5% gain
-    1   -  0.5 to 5% gain
-    0   -  0.5% loss to 0.5% gain
-    -1  -  2.5% to 0.5% loss
-    -2  -  worse than 2.5% loss  
-    
-    """
-    symbol_performances = {}
-    for symbol in symbol_list:
-        morning_price = float(current_data[symbol]['price'])
-        evening_price = float(new_data[symbol])
-        try:
-            price_change = (evening_price - morning_price) / morning_price
-        except:
-            price_change = 0.0
-        if price_change > 0.05:
-            performance = 2
-        elif price_change > 0.005:
-            performance = 1
-        elif price_change > 0.005:
-            performance = 0
-        elif price_change >= 0.025:
-            performance = -1
-        elif price_change < 0.025:
-            performance = -2
-        symbol_performances[symbol] = performance
-    return symbol_performances
-
-def update_past_data(current_data, symbol_performances):
-    """Append current day's data and performance to past data.  Past data
-    represents training data."""
-    past_data = open('past_data.txt', 'a')
-    for symbol in symbol_list:
-        for k, v in current_data[symbol].items():
-            past_data.write(k)
-            past_data.write(',')
-            past_data.write(v)
-            past_data.write(';')
-        past_data.write('performance,')
-        past_data.write(str(symbol_performances[symbol]))
-        past_data.write('\n')
-    past_data.close()
-
-def get_past_data():
-    """Read past data from file into feature and class lists for training."""
-    training_data = []
-    performance_data = []
-    past_data = open('past_data.txt', 'r')
-    for line in past_data:
-        line = line.split(';')
-        symbol_data = []
-        for data_element in line:
-            data_element = data_element.split(',')
-            value = data_element[1]
-            symbol_data.append(value)
-        
-        # move performance value (last value on line) from symbol data to
-        # performance data
-        performance_data.append(int(symbol_data.pop().strip('\n')))
-        training_data.append(symbol_data)
-    past_data.close()
-    return training_data, performance_data
+seattle = DecisionTreeColony(100, past_data, past_performance)
--- picker_classes.py
+++ picker_classes.py
@@ -12,22 +12,64 @@ class Picker():
     """Superclass for all pickers."""
     
     def __init__(self):
-        None
+        self.total_score = 0
     
     def pick(self):
         None
 
-class DecisionTreePicker(Picker()):
+class Colony():
+    """Superclass for all colonies."""
+
+class DecisionTreePicker(Picker):
     """Picker implementing simple decision tree classifier.  May be created
     from two parents, from a string, or randomly."""
     
-    def __init__(self, past_data):
+    def __init__(self):
+        self.age = 0
+        self.total_score = 0
         self.alg = tree.DecisionTreeClassifier()
-        self.alg = self.alg.fit(past_data)
+    
+    def train(self, past_data, past_performance):
+        self.alg = self.alg.fit(past_data, past_performance)
     
     def pick(self, current_data):
+        self.age += 1
         self.current_pick = self.alg.predict(current_data)
         return self.current_pick
     
     def evaluate_current_pick(self, new_data):
         self.current_score = None
+        self.total_score = (self.total_score * (self.age - 1) + self.current_score) / self.age
+    
+    def get_current_score(self):
+        return self.current_score
+    
+    def get_total_score(self):
+        return self.total_score
+
+class DecisionTreeColony():
+    """fill in later"""
+    
+    def __init__(self, size, past_data, past_performance):
+        self.pickers = []
+        for i in range(size):
+            picker = DecisionTreePicker()
+            picker.train(past_data, past_performance)
+            self.pickers.append(picker)
+    
+    def pick(self, current_data):
+        picks = []
+        for picker in self.pickers:
+            picks.append(picker.pick(current_data))
+    
+    def evaluate_pickers(self):
+        None
+    
+    def cull(self):
+        for picker in self.pickers:
+            if picker.get_total_score < 0:
+                self.pickers.remove(picker)
+                
+    def breed(self):
+        # don't forget to train new pickers        
+        None
